510f24af3c8e297bd74051e2bfc76c5bfedb0993
Rebecca <rbjanis@gmail.com>
2020-02-05 14:40:05 -0500

    Drake plan through included centers

--- R/drake-plan.R
+++ R/drake-plan.R
@@ -1,16 +1,25 @@
-library(drake)
-library(tidyverse)
+source("R/packages.R")
+source("R/funs.R")
 
-plan <- drake_plan(
-  raw_pre_data = load("data/2013-2015_TI_2019-02-05.RDa"),
-  raw_post_data = load("data/2016-2018_TI_2019-03-12.rda")
-  # data = raw_data %>%
-  #   mutate(Species = forcats::fct_inorder(Species)),
-  # hist = create_plot(data),
-  # fit = lm(Sepal.Width ~ Petal.Width + Species, data),
-  # report = rmarkdown::render(
-  #   knitr_in("report.Rmd"),
-  #   output_file = file_out("report.html"),
-  #   quiet = TRUE
-  # )
+plan <- drake::drake_plan(
+  raw_pre_data = target(
+    CCMHr::loadRDa("data/2013-2015_TI_2019-02-05.RDa"),
+    format = "fst"),
+  raw_post_data = target(
+    CCMHr::loadRDa("data/2016-2018_TI_2019-03-12.rda"),
+    format = "fst"),
+  combined_TI_data = target(
+    combine_TI_data(raw_pre_data, raw_post_data),
+    format = "fst"),
+  ccaps_update_dates = get_update_dates(combined_TI_data),
+  updated_centers = get_updated_centers(ccaps_update_dates),
+  included_centers = intersect(unique(combined_TI_data$CcmhID), updated_centers$CcmhID)
 )
+
+drake_config(plan) %>%
+  vis_drake_graph()
+
+make(plan)
+build_times(type = "build")
+
+included_centers <- readd(included_centers)
---
+++ R/packages.R
@@ -0,0 +1,8 @@
+# Setup
+
+library(tidyverse)
+library(drake)
+library(CCMHr)
+library(beepr)
+library(usethis)
+conflicted::conflict_prefer("filter", "dplyr")
